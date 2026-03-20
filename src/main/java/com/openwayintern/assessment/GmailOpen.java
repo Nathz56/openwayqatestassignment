69bd65b0b97e33d558eb4ddeb1c5ccbc16a9395f
Yonathan Hilkia <yonathanhilkia54@gmail.com>
2026-03-20 11:30:08 +0700

    feat(pw): added .env and .env dependencies then moved password to .env for safety

--- src/main/java/com/openwayintern/assessment/GmailOpen.java
+++ src/main/java/com/openwayintern/assessment/GmailOpen.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import io.github.cdimascio.dotenv.Dotenv;
+
 import java.time.Duration;
 
 public class GmailOpen {
@@ -28,6 +30,8 @@ public class GmailOpen {
     }
 
     public void loginGmail() {
+        Dotenv dotenv = Dotenv.configure().ignoreIfMalformed().ignoreIfMissing().load();
+        String password = dotenv.get("APP_PASS");
         
         driver.findElement(emailInput).sendKeys("krakensto2@gmail.com");
         driver.findElement(emailNextButton).click();
@@ -36,7 +40,7 @@ public class GmailOpen {
         wait.until(ExpectedConditions.elementToBeClickable(passField));
         
         passField.clear();
-        passField.sendKeys("123123-OKOK");
+        passField.sendKeys(password);
         driver.findElement(passwordNextButton).click();
     }
 }
