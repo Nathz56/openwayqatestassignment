034bdba9bd76a88702379f3870f09177d576cebe
Yonathan Hilkia <yonathanhilkia54@gmail.com>
2026-03-20 18:21:03 +0700

    deleted aftermethod import

--- src/test/java/com/openwayintern/assessment/ChromeOpen.java
+++ src/test/java/com/openwayintern/assessment/ChromeOpen.java
@@ -3,7 +3,6 @@ package com.openwayintern.assessment;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
